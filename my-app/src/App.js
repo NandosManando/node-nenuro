337b1ec69d4d578d12ceeb91934affaf9b13ff30
NandosManando <127309611+NandosManando@users.noreply.github.com>
2023-05-10 19:53:49 +0200

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,115 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <form className="wrapper">
+            <h2>Sign Up</h2>
+            <section className="group">
+                <input
+                    type="text"
+                    size="30"
+                    className="input"
+                    name="email"
+                    required
+                />
+                <label htmlFor="email" className="label">
+                    Name Of Company
+                </label>
+            </section>
+            <section className="group">
+                <input
+                    type="text"
+                    size="30"
+                    className="input"
+                    name="email"
+                    required
+                />
+                <label htmlFor="email" className="label">
+                    Name Of Employer
+                </label>
+            </section>
+            <section className="group">
+                <input
+                    type="text"
+                    size="30"
+                    className="input"
+                    name="email"
+                    required
+                />
+                <label htmlFor="email" className="label">
+                    Position
+                </label>
+            </section>
+            <section className="group">
+                <input
+                    type="text"
+                    size="30"
+                    className="input"
+                    name="email"
+                    required
+                />
+                <label htmlFor="email" className="label">
+                    Location
+                </label>
+            </section>
+            <section className="group">
+                <input
+                    type="text"
+                    size="30"
+                    className="input"
+                    name="email"
+                    required
+                />
+                <label htmlFor="email" className="label">
+                    Salary
+                </label>
+            </section>
+            <section className="group">
+                <input
+                    type="text"
+                    size="30"
+                    className="input"
+                    name="email"
+                    required
+                />
+                <label htmlFor="email" className="label">
+                    Create Username
+                </label>
+            </section>
+            <section className="group">
+                <input
+                    type="text"
+                    size="30"
+                    className="input"
+                    name="email"
+                    required
+                />
+                <label htmlFor="email" className="label">
+                    Create Password
+                </label>
+            </section>
+            <section className="group">
+                <input
+                    type="text"
+                    size="30"
+                    className="input"
+                    name="email"
+                    required
+                />
+                <label htmlFor="email" className="label">
+                    Confirm Password
+                </label>
+            </section>
+            <button 
+            onClick={() => ''} 
+            class="btn btn-primary mb-5">SUBMIT
+      </button>
+      <span className="footer"></span>
+        </form>
+  );
+}
+
+
+
+export default App;
